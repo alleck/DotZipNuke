0bbf978fa2472818d13816fe50d96162dc993f1f
alleck <kaleb.lewis92@gmail.com>
2014-04-17 22:27:56 -0500

    Update nuke.sh

--- nuke.sh
+++ nuke.sh
@@ -1,8 +1,9 @@
 echo "Finding Akeeba Backups..."
-find /home*/*/public_html/develop/administrator/components/com_akeeba/backup/ -name '*.zip*' >> backupsfound.log
+find /home*/*/public_html/*/administrator/components/com_akeeba/backup/ -name '*.zip*' >> backupsfound.log
 find /home*/*/public_html/administrator/components/com_akeeba/backup/  -name '*.zip*' >> backupsfound.log
 echo "Finding Backup Buddy Backups..."
 find /home*/*/public_html/wp-content/uploads/backupbuddy_backups/ -name '*.zip' >> backupsfound.log
+find /home*/*/public_html/*/wp-content/uploads/backupbuddy_backups/ -name '*.zip' >> backupsfound.log
 echo "Finding Large Zip files. (DotZipNuke)..."
 find /home*/* -type f -name '*.zip*' -size +500M >> backupsfound.log
 echo "Done! Check backupsfound.log"
